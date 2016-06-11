3b20122d69d56d1b3b5f5623df5b05b76cc65076
Egor Lakomkin <egor.lakomkin@gmail.com>
2016-06-11 22:51:42 +0200

    extract noun phrases from reviews

--- extract_nouns.py
+++ extract_nouns.py
@@ -1,14 +1,23 @@
 from data_utils import load_booking_reviews
 import spacy
+from collections import defaultdict
 
+filter_list = [ "i", "we", "it", "us", "you", "they", "she"]
 
 if __name__ == "__main__":
     reviews = load_booking_reviews()
     nlp = spacy.en.English()
+
+    freq_stats = defaultdict( float )
+
     for review in reviews:
         review_text = review.text
 
         doc = nlp( review_text, entity = False )
 
         for np in doc.noun_chunks:
-            print np.text
+            np_text = np.text.lower()
+            if np_text not in filter_list:
+                freq_stats[ np_text ] += 1
+
+    num_docs = len(reviews)
