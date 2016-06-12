19be90391fbb7091c47a11b02979904d10f3b454
Egor Lakomkin <egor.lakomkin@gmail.com>
2016-06-12 15:00:06 +0200

    syntax tree traversal to extract noun phrase + adjective patterns

--- extract_nouns.py
+++ extract_nouns.py
@@ -2,7 +2,32 @@ from data_utils import load_booking_reviews
 import spacy
 from collections import defaultdict
 
-filter_list = [ "i", "we", "it", "us", "you", "they", "she"]
+np_root_filter_list = [ "i", "we", "it", "you", "they", "she", "he"]
+
+
+
+def node_acomp_filter(node):
+    if node.dep_ == "acomp":
+        return True
+    return False
+
+def filter_noun_phrase(noun_phrase, doc):
+    root = noun_phrase.root
+    if root.lemma_ in np_root_filter_list:
+        return False
+    if root.dep_ != "nsubj":
+        return False
+
+    return True
+
+def get_candidate_sentiment_phrases( doc, noun_phrase ):
+    root = noun_phrase.root
+    res = []
+    for node in  root.head.subtree:
+        if node_acomp_filter( node ):
+            res.append( (list(root.head.subtree),np,  node) )
+
+    return res
 
 if __name__ == "__main__":
     reviews = load_booking_reviews()
@@ -16,8 +41,12 @@ if __name__ == "__main__":
         doc = nlp( review_text, entity = False )
 
         for np in doc.noun_chunks:
-            np_text = np.text.lower()
-            if np_text not in filter_list:
-                freq_stats[ np_text ] += 1
+
+            if filter_noun_phrase( np, doc ):
+                acomp_nodes = get_candidate_sentiment_phrases( doc, np )
+                if len(acomp_nodes) > 0:
+                    for phrase, np, acomp_node in acomp_nodes:
+                        print np, "->", acomp_node
+                        print phrase
 
     num_docs = len(reviews)
